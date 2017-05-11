8731df3eee70f65a1056c6c1b08b5eb9f33b6090
efaxb <efaxb@132.238.232.162>
2017-05-10 21:50:40 -0400

    Try #2

--- src/com/mycompany/section2/MainEntry.java
+++ src/com/mycompany/section2/MainEntry.java
@@ -10,7 +10,7 @@ public class MainEntry {
 		// TODO Auto-generated method stub
 	
 		// initialize
-		MyGenInterface<Integer,String> mgi1 = new MyGenClass<Integer,String>(1, "yourName");
+		MyGenInterface<Integer,String> mgi1 = new MyGenClass<Integer,String>(1, "Andrew");
 		MyGenInterface<Integer,Integer> mgi2 = new MyGenClass<Integer,Integer>(1,2017);
 		
 		// assignment
@@ -18,10 +18,12 @@ public class MainEntry {
 		Float f1 = MyRegularClass.getSum(100f,200f);
 	
 		// output
-		System.out.println(mgi1);
-		System.out.println(mgi2);
-		System.out.println(i1);
-	    System.out.println(f1);
+		System.out.println("mgi1 key"+mgi1.getKey());
+		System.out.println("mgi1 value"+mgi1.getValue());
+		System.out.println("mgi2 key:" +mgi2.getKey());
+		System.out.println("mgi2 value:" +mgi2.getValue());
+		System.out.println("i1 "+ i1);
+	    System.out.println("f1 " +f1);
 		
 	}
 
